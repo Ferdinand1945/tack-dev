0b78b04978347406684a8a7bb98caf4b4e2be1b1
Rikard Bartholf <rikard@bartholf.nu>
2018-11-14 08:39:12 +0100

    Remove more unused files

--- dbConf.php
+++ dbConf.php
@@ -7,10 +7,10 @@ defined("DB_SERVER") ? null : define("DB_SERVER", "localhost");
 defined("DB_NAME") ? null: define("DB_NAME", "tack");
 
 /** MySQL database username */
-defined("DB_USER") ? null : define ("DB_USER", "phpmyadmin");
+defined("DB_USER") ? null : define ("DB_USER", "dev");
 
 /** MySQL database password */
-defined("DB_PASSWORD") ? null : define ("DB_PASSWORD", "tack2018");
+defined("DB_PASSWORD") ? null : define ("DB_PASSWORD", "dev");
 
 /** Database Charset to use in creating database tables. */
 defined('DB_CHARSET') ? null : define('DB_CHARSET', 'utf8');
